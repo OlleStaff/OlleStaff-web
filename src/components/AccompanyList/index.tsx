a7c22e4117c1279d0277a1c098699f005e75a8a2
hoon-hoon <kc0393@naver.com>
2025-07-06 16:39:03 +0900

    feat: 동행 컴포넌트에 무한스크롤 기능 추가 및 동행리스트 페이지,훅 적용

--- src/components/AccompanyList/index.tsx
+++ src/components/AccompanyList/index.tsx
@@ -1,17 +1,33 @@
 import styled from "@emotion/styled";
 import { AccompanyListItem } from "./AccompanyListItem";
 import { AccompanyListItemProps } from "@/types/accompany";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { SkeletonList } from "../Skeleton/SkeletonList";
 
 interface AccompanyListProps {
     data: AccompanyListItemProps[];
+    fetchNextPage?: () => void;
+    hasNextPage?: boolean;
+    isFetchingNextPage?: boolean;
 }
 
-export const AccompanyList = ({ data }: AccompanyListProps) => {
+export const AccompanyList = ({ data, fetchNextPage, hasNextPage, isFetchingNextPage }: AccompanyListProps) => {
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView && hasNextPage && fetchNextPage) {
+            fetchNextPage();
+        }
+    }, [inView, hasNextPage, fetchNextPage]);
+
     return (
         <Wrapper>
             {data.map((item, index) => (
                 <AccompanyListItem key={index} {...item} />
             ))}
+            {hasNextPage && <div ref={ref} style={{ height: 1 }} />}
+            {isFetchingNextPage && <SkeletonList variant="accompany" count={3} />}
         </Wrapper>
     );
 };
