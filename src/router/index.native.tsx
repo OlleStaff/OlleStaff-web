338dd003d2a9f0d1b8487a42861030499bdb32d6
hoon-hoon <kc0393@naver.com>
2025-08-12 01:43:02 +0900

    update: 안쓰는 파일 삭제

--- src/router/index.native.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import AuthRoutes from "./AuthRoutes.native";
-import OwnerRoutes from "./OwnerRoutes";
-import StaffRoutes from "./StaffRoutes";
-
-console.log("native 버전 index파일 성공");
-
-const router = createBrowserRouter([...AuthRoutes, ...OwnerRoutes, ...StaffRoutes]);
-export default router;
