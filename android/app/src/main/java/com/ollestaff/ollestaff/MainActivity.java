9cca4903d174373f5f52adb9350a753717baaac0
hoon-hoon <kc0393@naver.com>
2025-08-25 16:37:19 +0900

    feat: android 카카오 SDK 추가 및 설정

--- android/app/src/main/java/com/ollestaff/ollestaff/MainActivity.java
+++ android/app/src/main/java/com/ollestaff/ollestaff/MainActivity.java
@@ -2,4 +2,14 @@ package com.ollestaff.ollestaff;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import com.kakao.sdk.common.KakaoSdk;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, getString(R.string.kakao_app_key));
+    }
+}
